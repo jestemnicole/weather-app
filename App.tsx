3fb28605156f9cd9a0af230b3d3c907d51c5ed6b
nicolynicole <107006884+nicolynicole@users.noreply.github.com>
2023-06-26 02:01:29 -0400

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
-import type {PropsWithChildren} from 'react';
+import  {PropsWithChildren} from 'react';
 import FRONTEND_URL from './config/config.js';
 import axios from 'axios';
 import Geolocation from '@react-native-community/geolocation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 import {
   SafeAreaView,
@@ -20,216 +23,36 @@ import {
   PermissionsAndroid
 } from 'react-native';
 
-type locationInterface = {
-  latitude : number,
-  longitude : number
-}
+import Home from './components/Home';
+import WeatherPage from './components/WeatherPage';
+
+type StackParamList = {
+  Home: undefined;
+  WeatherPage: {location : string};
+};
+
+const Stack = createNativeStackNavigator<StackParamList>();
 
 
 function App(): JSX.Element {
  
-  const [currentWeatherData, setCurrentWeatherData] = useState<any>(null);
-  const [hourlyWeatherData, setHourlyWeatherData] = useState<any>(null);
-  const [weeklyWeatherData, setWeeklyWeatherData] = useState<any>(null);
-  const [airQualityData, setAirQualityData] = useState('');
-  const [uvIndexData, setUvIndexData] = useState('');
-  const [location, setLocation] = useState<locationInterface | null>(null);
-
-    async function requestLocation() {
-       try {
-          const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            {
-              title : 'Location Permission',
-              message : 'tell me coords ',
-              buttonPositive : 'SURE',
-            });
-
-            if (granted === PermissionsAndroid.RESULTS.GRANTED){
-              console.log('ok permission granted')
-            }else{
-              console.log('ok so no coords')
-            }
-
-       }catch(e){
-          console.error(e)
-       }
-    }
-
-    Geolocation.getCurrentPosition( position => {
-
-        const currentPosition = {
-          latitude : position.coords.latitude,
-          longitude : position.coords.longitude
-        };
-
-        setLocation(currentPosition);
-        
-    },
-    error => {
-      console.log('Error: ' , error.message);
-    }
-    );
-
-    
-   
-    async function fetchWeatherData(url : string, setWeatherData : React.Dispatch<any>) {
-        try {
-         
-          const response = await axios(url);
-          setWeatherData(response.data);
-          
-        } catch (e){
-          console.error(e);
-        }
-    }
-
-    useEffect(() => {
-
-        requestLocation();
-      
-        const currentWeatherURL = `${FRONTEND_URL}/current/${location?.latitude},${location?.longitude}`;
-        const hourlyWeatherURL = `${FRONTEND_URL}/hourly/${location?.latitude},${location?.longitude}`;
-        const weeklyWeatherURL = `${FRONTEND_URL}/weekly/${location?.latitude},${location?.longitude}`;
-        const airQualityURL = `${FRONTEND_URL}/airquality/${location?.latitude},${location?.longitude}`;
-        const uvURL = `${FRONTEND_URL}/uv/${location?.latitude},${location?.longitude}`;
-        
-        fetchWeatherData(currentWeatherURL, setCurrentWeatherData);
-        fetchWeatherData(hourlyWeatherURL, setHourlyWeatherData);
-        fetchWeatherData(weeklyWeatherURL, setWeeklyWeatherData);
-        fetchWeatherData(airQualityURL, setAirQualityData);
-        fetchWeatherData(uvURL, setUvIndexData);
-        
-      }, []);
-
-    type weeklyWeatherInterface = {
-      day : string,
-      icon : string,
-      low : number,
-      high : number
-    }
-      
   return ( 
-    <ImageBackground source={require('./assets/blue_sky.jpg')}>
-    <SafeAreaView>
-      <ScrollView>
-      {currentWeatherData && <View style={styles.currentWeatherDisplay}>
-        <Text style={{fontSize: 40,
-                      marginTop: 30,
-                      color : 'white', fontFamily : 'Roboto', textShadowColor : 'rgba(0, 0, 0, 0.6)', textShadowRadius : 1, textShadowOffset : {width : -1, height : 1}}}>{currentWeatherData.location.name}</Text>
-        <Text style={{fontSize: 80, color : 'white', fontFamily : 'Roboto', textShadowColor : 'rgba(0, 0, 0, 0.6)', textShadowRadius : 1, textShadowOffset : {width : -1, height : 1}}}>{Math.round(currentWeatherData.current.temp_f)}°</Text>
-        <Text style={{fontSize: 20, color : 'white', marginBottom: 10, textShadowColor : 'rgba(0, 0, 0, 0.6)', textShadowRadius : 1, textShadowOffset : {width : -1, height : 1}}}>{currentWeatherData.current.condition.text}</Text>
-      </View>}
-
-    {<FlatList style={styles.hourlyWeatherList} data={hourlyWeatherData}
-              renderItem={({item}) => 
-                <View style={styles.hourlyWeatherDisplay}>
-                  <Text style={{color : 'white', fontFamily: 'Roboto', fontSize : 15}}>{item.time}</Text>
-                  <Image source={{uri : 'https:' + item.icon}}
-                          style={{width: 40, height: 40}}></Image>
-                  <Text style={{color : 'white', fontFamily: 'Roboto', fontSize : 20}}>{item.temp_f}</Text>
-                </View>}
-               horizontal={true}>
-      </FlatList>}
-
-      <View style={styles.weeklyWeatherList}>
-            <Text style={{color : 'white', fontFamily : 'Roboto', fontSize : 13}}>3-DAY FORECAST</Text>
-      {weeklyWeatherData && weeklyWeatherData.map((item : weeklyWeatherInterface) => {
-              return (
-              <View key={item.day} style={styles.weeklyWeatherDisplay}>
-                <Text style={styles.weeklyWeatherText}>{item.day}</Text>
-                <Text style={{...styles.weeklyWeatherText, marginLeft : 'auto', marginRight : 30}}>L: {item.low}°</Text>
-                <Text style={styles.weeklyWeatherText}>H: {item.high}°</Text>
-              </View>
-              )
-              
-      })}
-      </View>
-         
-      <View style={{flex : 1, flexDirection : 'row'}}>
-          <View style={styles.UVIndexView}>
-          <Text style={{color : 'white'}}>UV INDEX</Text>
-          <Text style={{color : 'white', fontSize : 20, textAlign: 'center'}}>{uvIndexData}</Text>
-          </View>
-
-          <View style={styles.airQualityView}>
-        <Text style={{color : 'white', fontFamily : 'Roboto', fontSize : 13}}>AIR QUALITY</Text>
-        <Text style={{color : 'white', fontFamily : 'Roboto', fontSize : 20, textAlign : 'center'}}>{airQualityData}</Text>
-        </View>
-
-          
-        </View>
-         
-
-          
-         
-      </ScrollView>
+    
+    <SafeAreaView style={{flex : 1}}>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen name='Home' component={Home}></Stack.Screen>
+      <Stack.Screen name='WeatherPage' component={WeatherPage} />
+      </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaView>
-    </ImageBackground>
+   
   );
 }
 
 const styles = StyleSheet.create({
 
   
-
-  currentWeatherDisplay : {
-  
-    alignItems: 'center',
-    fontFamily: 'Roboto',
-    marginBottom : 20
-  },
-
-  hourlyWeatherList : {
-    backgroundColor : 'rgba(0, 0, 0, 0.1)',
-    marginLeft : 18,
-    marginRight : 18,
-    borderRadius : 10,
-    marginBottom : 10,
-    padding : 8
-  },
-
-  hourlyWeatherDisplay : {
-    alignItems: 'center',
-    marginRight : 18
-
-  },
-
-  weeklyWeatherDisplay : {
-    flexDirection : 'row',
-    marginBottom : 10,
-    marginTop : 10
-  },
-  weeklyWeatherList : {
-    backgroundColor : 'rgba(0, 0, 0, 0.1)',
-    marginLeft : 18,
-    marginRight : 18,
-    borderRadius : 10,
-    marginBottom : 10,
-    padding : 8
-  },
-  weeklyWeatherText : {
-    color : 'white',
-    fontSize : 20,
-    fontFamily : 'Roboto'
-  },
-
-  airQualityView : {
-    backgroundColor : 'rgba(0, 0, 0, 0.1)',
-    marginLeft : 18,
-    marginRight : 18,
-    marginBottom : 10,
-    padding : 8,
-    borderRadius : 10
-  },
-
-  UVIndexView : {
-    backgroundColor : 'rgba(0, 0, 0, 0.1)',
-    marginLeft : 18,
-    marginRight : 18,
-    padding : 8,
-    borderRadius : 10,
-    marginBottom : 10,
-  }
 
 });
 
